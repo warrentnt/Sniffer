168cc15c6f9e252dda3eab1a979907d49813c565
Warren Thompson <warrentnt@gmail.com>
2022-06-12 22:15:47 -0400

    Updated project code

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Sniffer',
+    version='',
+    packages=[''],
+    url='',
+    license='Free to use and modify',
+    author='Warren Thompson',
+    author_email='Warren.Thompson@trojans.dsu.edu',
+    description='Simple Python Sniffer for HTTP Requests and clear text username and passwords'
+)
